51f3d39988563bf1c1abe502659cc2b2d95acd97
Aranka Bullen <aranka.bullen@tlgcohort.com>
2023-10-31 17:54:05 -0400

    Lab01.3: complete

--- Lab01.2/src/com/entertainment/Television.java
+++ Lab01.2/src/com/entertainment/Television.java
@@ -1,5 +1,9 @@
 package com.entertainment;
 
+import java.util.Objects;
+import  java.util.HashSet;
+import java.util.Set;
+
 public class Television {
     //fields
     private String brand;
@@ -40,6 +44,35 @@ public class Television {
 
     public void  changeChannel(int channel) {
         tuner.setChannel(channel);
+    }
+
+    @Override
+    public int hashCode(){
+        /*
+         *This is a poorly written has function, because it easily yields "hash collisions."
+         * A hash collisionis when "different" objects have the same hash code (just by coincident).
+         */
+         //return getBrand().length() + getVolume();
+
+         //Instead, we rely on Objects.has() to give us a "scientifically correct" hash function.
+        return Objects.hash(getBrand(), getVolume());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean result = false;
+
+        //proceed  only if 'obj' is really referencing a Television object.
+        if(obj instanceof Television) {
+
+            //safely downcast 'obj' to more specific reference type Television
+            Television other = (Television) obj;
+
+            // do the checks: business equality is defined by brand, volume being the same
+            result= Objects.equals(this.getBrand(), other.getBrand()) &&  // this a null safe method (Object.equals())
+                    this.getVolume()==other.getVolume();  // primitives cannot be null
+        }
+        return result;
     }
 
     //toString
--- Lab01.2/src/com/entertainment/client/TelevisionClient.java
+++ Lab01.2/src/com/entertainment/client/TelevisionClient.java
@@ -2,20 +2,30 @@ package com.entertainment.client;
 
 import com.entertainment.Television;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class TelevisionClient {
     public static void main(String[] args) {
         Television tv = new Television("RCA", 30);
-        System.out.println(tv);
-        System.out.println();
 
         Television tv2 = new Television();
-        System.out.println(tv2);
         tv2.changeChannel(25);
         System.out.println(tv2);
 
 
         Television tv3 = new Television("RCA", 30);
-        System.out.println(tv==tv3);
-        System.out.println(tv.equals(tv3));
+        System.out.println("tv == tv3 " + (tv==tv3));
+        System.out.println("tv.equals(tv3)" + tv.equals(tv3));
+
+
+        System.out.println(tv.hashCode());  //745160567
+        System.out.println(tv3.hashCode());   //610984013
+        System.out.println();
+
+        Set<Television> tvs = new HashSet<>();
+        tvs.add(tv);
+        tvs.add(tv3);
+        System.out.println("the size if the set is: " + tvs.size());
     }
 }
