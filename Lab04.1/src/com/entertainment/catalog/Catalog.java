f46c4edafdede9ef9bb699021c53325969fbe17b
Aranka Bullen <aranka.bullen@tlgcohort.com>
2023-11-03 15:39:30 -0400

    Lab04.1: Initial work [2023-11-03 @ 3:30 EST]

--- Lab04.1/src/com/entertainment/catalog/Catalog.java
+++ Lab04.1/src/com/entertainment/catalog/Catalog.java
@@ -28,7 +28,13 @@ public class Catalog {
    * A no-matches result should be an empty collection (not null).
    */
   public static Collection<Television> findByBrand(String brand) {
-    return null;
+    Collection<Television> result = new ArrayList<>();
+    for(Television tv: catalog) {
+      if(tv.getBrand().equals(brand)){
+        result.add(tv);
+      }
+    }
+    return result;
   }
   
   /**
@@ -37,8 +43,17 @@ public class Catalog {
    * A no-brands-passed result should be an empty map (not null).
    */
   public static Map<String,Collection<Television>> findByBrands(String... brands) {
-    return null;
+    Map<String, Collection<Television>> result = new HashMap<>();
+
+
+   // Collection<Television> tvList = new ArrayList<>();  //alternative method to findbybrand
+    for (String brand : brands) {
+      Collection<Television> tvList = findByBrand(brand);
+      result.put(brand, tvList);
+    }
+    return result;
   }
+
 
   /**
    * Returns entire catalog.
@@ -52,7 +67,7 @@ public class Catalog {
    *  This is an all-static utility class, not the java.util.Collection interface.
    */
   public static Collection<Television> getInventory() {
-    return catalog;
+    return Collections.unmodifiableCollection(catalog);
   }
 
   /*
--- Lab04.1/test/com/entertainment/catalog/CatalogTest.java
+++ Lab04.1/test/com/entertainment/catalog/CatalogTest.java
@@ -10,14 +10,65 @@ package com.entertainment.catalog;
 
 import static org.junit.Assert.*;
 import java.util.Collection;
+import java.util.Map;
+
 import org.junit.Test;
 import com.entertainment.Television;
 
 public class CatalogTest {
-  
+
+
+
+
   /**
    * Contract: a no-matches result should be an empty collection (not null).
    */
+
+  @Test
+  public void getInventory_shouldReturnReadOnlyCollection() {
+    Collection<Television>tvs =Catalog.getInventory();
+    tvs.clear(); // should throw exception
+  }
+  @Test
+  public void findByBrands_shouldReturnEmptyMap_whenNoBrandPassed() {
+    Map<String,Collection<Television>> tvMap = Catalog.findByBrands();
+
+    assertTrue(tvMap.isEmpty());
+  }
+  @Test
+  public void findBYBrand_shouldReturnPopulatedMap_whenBrandsPassed2() { //alternative method to test
+   String[] brands = {"Sony", "Zenith", "RCA", "Magnavox"};
+    Map<String, Collection<Television>> tvMap = Catalog.findByBrands(brands);
+
+    assertEquals(brands.length, tvMap.size());
+
+    for(String brand: brands) {
+      verifyCollection(brand, tvMap.get(brand));
+    }
+  }
+
+
+  @Test
+  public void findBYBrand_shouldReturnPopulatedMap_whenBrandsPassed() {
+    Map<String, Collection<Television>> tvMap = Catalog.findByBrands("Sony", "Zenith");
+    assertEquals(2, tvMap.size());
+
+    Collection<Television> sonyTvs = tvMap.get("Sony");
+    for(Television tv : sonyTvs) {
+      assertEquals("Sony", tv.getBrand());
+    }
+    Collection<Television> zenithTvs = tvMap.get("Zenith");
+    for(Television tv : zenithTvs) {
+      assertEquals("Zenith", tv.getBrand());
+    }
+  }
+
+  private void verifyCollection(String brand, Collection<Television> tvs) {
+    for(Television tv : tvs){
+      assertEquals(brand, tv.getBrand());
+    }
+  }
+
   @Test
   public void testFindByBrandNoMatches() {
     Collection<Television> tvs = Catalog.findByBrand("NO-MATCHES");
