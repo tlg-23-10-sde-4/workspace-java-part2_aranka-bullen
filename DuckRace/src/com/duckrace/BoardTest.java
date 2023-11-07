0d7925aa801faef007a85d87465cd22e2a0c5dc9
Aranka Bullen <aranka.bullen@tlgcohort.com>
2023-11-07 12:49:40 -0500

    DuckRacer: Iteration 2 (studentIdMap, racerMap)

---
+++ DuckRace/src/com/duckrace/BoardTest.java
@@ -0,0 +1,18 @@
+package com.duckrace;
+import static com.duckrace.Reward.*;
+class BoardTest {
+
+    public static void main(String[] args) {
+        Board board = new Board();
+        //board.dumpStudentIdMap();
+
+        board.update(4, Reward.DEBIT_CARD);
+        board.update(4, DEBIT_CARD);
+        board.update(14, DEBIT_CARD);
+        board.update(14, PRIZES);
+        board.update(1, PRIZES);
+
+        board.show();
+
+    }
+}
